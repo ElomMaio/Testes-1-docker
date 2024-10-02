0577e07c4d18c6aebdcafe1076e465f6a6c6dd9e
ElomMaio <maioelom@gmail.com>
2024-10-02 16:45:29 -0300

    Update init2.sql

--- init2.sql
+++ init2.sql
@@ -1,14 +1,12 @@
 CREATE TABLE tbl_formularios (
-    a.a VARCHAR PRIMARY KEY,
-    b.b TEXT,
-    c.c VARCHAR
-    );
+    a VARCHAR PRIMARY KEY,
+    b TEXT,
+    c VARCHAR
+);
 
 CREATE TABLE forms (
-    b.a TEXT UNIQUE NOT NULL PRIMARY KEY,
-    b.b VARCHAR UNIQUE NOT NULL,
-    b.c VARCHAR,
-    CONSTRAINT b.b
-        FOREIGN KEY (b.b)
-        REFERENCES tbl_formularios(b.b)
+    a TEXT UNIQUE NOT NULL PRIMARY KEY,
+    b VARCHAR UNIQUE NOT NULL,
+    c VARCHAR,
+    FOREIGN KEY (b) REFERENCES tbl_formularios(b)
 );

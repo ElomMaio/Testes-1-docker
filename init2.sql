ee2fe3d33df837ff05cfdeaa916c167c7391434e
ElomMaio <maioelom@gmail.com>
2024-10-02 14:48:13 -0300

    Update init2.sql

--- init2.sql
+++ init2.sql
@@ -8,16 +8,16 @@ CREATE DATABASE ELOM
     LC_CTYPE = 'en_US.utf8';
 
 CREATE TABLE tbl_formularios (
-    1.1 VARCHAR PRIMARY KEY,
-    2.1 TEXT,
-    3.1 VARCHAR
+    a.1 VARCHAR PRIMARY KEY,
+    b.1 TEXT,
+    c.1 VARCHAR
     );
 
 CREATE TABLE forms (
-    1.2 TEXT UNIQUE NOT NULL PRIMARY KEY,
-    2.2 VARCHAR UNIQUE NOT NULL,
-    3.2 VARCHAR,
-    CONSTRAINT 2.1
-        FOREIGN KEY (2.1)
-        REFERENCES tbl_formularios(2.1)
+    a.2 TEXT UNIQUE NOT NULL PRIMARY KEY,
+    b.2 VARCHAR UNIQUE NOT NULL,
+    c.2 VARCHAR,
+    CONSTRAINT b.1
+        FOREIGN KEY (b.1)
+        REFERENCES tbl_formularios(b.1)
 );

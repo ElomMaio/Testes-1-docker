3b97e361c3bf326491d05b8eb786cc6dc45eeec4
ElomMaio <maioelom@gmail.com>
2024-10-03 13:50:46 -0300

    Update init2.sql

--- init2.sql
+++ init2.sql
@@ -1,15 +1,16 @@
-SET DATESTYLE TO PostgreSQL,European;
+SET DATESTYLE TO PostgreSQL, European;
 
-CREATE DATABASE 
+CREATE DATABASE my_database
     WITH 
     TEMPLATE = template0 
     ENCODING = 'UTF8' 
     LC_COLLATE = 'en_US.utf8' 
     LC_CTYPE = 'en_US.utf8';
 
+\c my_database 
 CREATE TABLE tbl_formularios (
     a VARCHAR PRIMARY KEY,
-    b TEXT,
+    b TEXT UNIQUE,  -- Alterado para UNIQUE
     c VARCHAR
 );
 
@@ -17,5 +18,5 @@ CREATE TABLE forms (
     a TEXT UNIQUE NOT NULL PRIMARY KEY,
     b VARCHAR UNIQUE NOT NULL,
     c VARCHAR,
-    FOREIGN KEY (b) REFERENCES tbl_formularios(b)
+    FOREIGN KEY (b) REFERENCES tbl_formularios(b)  
 );
